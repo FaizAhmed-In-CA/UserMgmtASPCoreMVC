3de4ff00ea55a58bb24e51aa056a15392733fa40
Faiz Ahmed <bdfaiz@gmail.com>
2022-12-19 19:54:28 +0600

    Initial commit

---
+++ UserMgmtASPCoreMVC/Enums/Roles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserMgmtASPCoreMVC.Enums
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
+
